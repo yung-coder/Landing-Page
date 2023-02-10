b517bf861e5ddb4a2c88416c7290e830c4f9f223
yung-coder <chandel.sanjeev5@gmail.com>
2023-02-11 00:36:10 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs,  Footer, Gallery, Header, Intro,  SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
@@ -11,9 +11,7 @@ const App = () => (
     <AboutUs />
     <SpecialMenu />
     <Intro />
-    <Laurels />
     <Gallery />
-    <FindUs />
     <Footer />
   </div>
 );
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,9 @@
 import SubHeading from './SubHeading/SubHeading';
-import Newsletter from './Footer/Newsletter';
-import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './Menuitem/MenuItem';
 import Navbar from './Navbar/Navbar';
 
 export {
   SubHeading,
-  Newsletter,
-  FooterOverlay,
   MenuItem,
   Navbar,
 };
